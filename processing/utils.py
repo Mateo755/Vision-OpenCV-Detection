40e3fae250cd0f0943edba224420367ab8ee4704
Mateo755 <mateusz.polska07@gmail.com>
2025-06-13 11:40:20 +0200

    testing_conditions

--- processing/utils.py
+++ processing/utils.py
@@ -1,14 +1,5 @@
-import cv2
+from processing.detection import detect_from_background_image
 
+def perform_processing(cap):
+    all_objects = detect_from_background_image(cap, show=True)
 
-def perform_processing(cap: cv2.VideoCapture) -> dict[str, int]:
-    # TODO: add video processing here
-    return {
-        "liczba_samochodow_osobowych_z_prawej_na_lewa": 5,
-        "liczba_samochodow_osobowych_z_lewej_na_prawa": 5,
-        "liczba_samochodow_ciezarowych_autobusow_z_prawej_na_lewa": 5,
-        "liczba_samochodow_ciezarowych_autobusow_z_lewej_na_prawa": 5,
-        "liczba_tramwajow": 5,
-        "liczba_pieszych": 5,
-        "liczba_rowerzystow": 5
-    }
